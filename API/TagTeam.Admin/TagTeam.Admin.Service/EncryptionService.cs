9afd39ec916d36748d642e8f0ca6cbbcd9649e0f
MihiranC <mihiranc.tagholds@gmail.com>
2020-08-23 12:46:53 +0530

    create Encryption Service

---
+++ API/TagTeam.Admin/TagTeam.Admin.Service/EncryptionService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using TagTeam.Admin.Domain;
+using TagTeam.Admin.Domain.CustomModels;
+using TagTeam.Admin.Service.Interfaces;
+
+namespace TagTeam.Admin.Service
+{
+    public class EncryptionService
+    {
+        
+    }
+}
