87a9bbcda5e0cbd726e1eb796eaa7f93d4f0577c
MihiranC <mihiranc.tagholds@gmail.com>
2021-10-03 20:25:26 +0530

    commit

---
+++ API/TagTeam.Admin/TagTeam.Admin.API/Controllers/Ref_CitiesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TagTeam.Admin.Domain;
+using TagTeam.Admin.Domain.CustomModels;
+using TagTeam.Admin.Service.Interfaces;
+
+namespace TagTeam.Admin.API.Controllers
+{
+    [Route("api/TGAdmin/[controller]")]
+    [ApiController]
+    public class Ref_CitiesController : ControllerBase
+    {
+        private readonly IRef_Cities_interface _service;
+
+        public Ref_CitiesController(IRef_Cities_interface service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("Select")]
+        public async Task<ActionResult> Select(int cityID)
+        {
+            var response = await _service.Select(cityID);
+            return Ok(response);
+        }
+    }
+}
---
+++ API/TagTeam.Admin/TagTeam.Admin.Domain/Ref_Cities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TagTeam.Admin.Domain
+{
+    public class Ref_Cities
+    {
+        public int cityID { get; set; }
+        public int districtID { get; set; }
+        public string name { get; set; }
+        public string postCode { get; set; }
+    }
+}
