47dd175ee344b46dcc003fde8fea01565a575613
MihiranC <mihiranc.tagholds@gmail.com>
2022-02-14 21:09:22 +0530

    commit

---
+++ API/TagTeam.Admin/TagTeam.Admin.API/Controllers/SSRSController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TagTeam.Admin.Service.Interfaces;
+using TagTeam.Admin.Domain;
+using System.IO;
+using Microsoft.Reporting.WebForms;
+using System.Net.Http;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace TagTeam.Admin.API.Controllers
+{
+    [Route("api/TGAdmin/[controller]")]
+    [ApiController]
+    public class SSRSController : ControllerBase
+    {
+
+        [HttpPost("DownloadSSRS")]
+        public SSRS DownloadSSRS(SSRS SSRSDetails)
+        {
+            try
+            {
+                Warning[] warnings;
+                string[] streamIds;
+                string mimeType = string.Empty;
+                string encoding = string.Empty;
+                string extension = string.Empty;
+
+                ReportViewer RV = new ReportViewer();
+
+                RV.ProcessingMode = ProcessingMode.Remote;
+                RV.ServerReport.ReportServerUrl = new Uri(SSRSDetails.Key_ReportServer);
+                RV.ServerReport.ReportPath = "/" + SSRSDetails.Key_ReportPath + "/" + SSRSDetails.Report;
+
+                string x = RV.ServerReport.ReportServerUrl + RV.ServerReport.ReportPath;
+
+                List<ReportParameter> paramaters = new List<ReportParameter>();
+
+                for (int i = 0; i < SSRSDetails.Parameters.Count; i++)
+                {
+                    paramaters.Add(new ReportParameter(SSRSDetails.Parameters[i].ParameterName, SSRSDetails.Parameters[i].ParameterValue));
+                }
+
+                RV.ServerReport.SetParameters(paramaters);
+
+                SSRSDetails.ReportOutput = RV.ServerReport.Render("PDF", null, out mimeType, out encoding, out extension, out streamIds, out warnings);
+
+                SSRSDetails.ReportOutputToBase64String = Convert.ToBase64String(SSRSDetails.ReportOutput);
+
+                List<SSRS> reprot = new List<SSRS>();
+                reprot.Add(SSRSDetails);
+
+                //return result;
+
+                //HttpResponseMessage responseSSRS = new HttpResponseMessage(HttpStatusCode.OK);
+               
+                //    responseSSRS.Content = new StreamContent(new MemoryStream(SSRSDetails.ReportOutput));
+                //    responseSSRS.Content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
+                //    responseSSRS.Content.Headers.ContentLength = SSRSDetails.ReportOutput.Length;
+                //    ContentDispositionHeaderValue contentDisposition = null;
+                //    if (ContentDispositionHeaderValue.TryParse("inline; filename=test.pdf", out contentDisposition))
+                //    {
+                //        responseSSRS.Content.Headers.ContentDisposition = contentDisposition;
+                //    }
+
+                return SSRSDetails;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+    }
+}
---
+++ API/TagTeam.Admin/TagTeam.Admin.Domain/SSRS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace TagTeam.Admin.Domain
+{
+    public class SSRS
+    {
+        public List<SSRSParameters> Parameters = new List<SSRSParameters>();
+        public string Key_ReportPath { get; set; }
+        public string Report { get; set; }
+        public string Key_ReportServer { get; set; }
+        public byte[] ReportOutput { get; set; }
+        public string ReportOutputToBase64String { get; set; }
+    }
+
+    public class SSRSParameters
+    {
+        public string ParameterName { get; set; }
+        public string ParameterValue { get; set; }
+    }
+}
